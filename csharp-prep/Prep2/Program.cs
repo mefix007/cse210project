e0a563fb67459513dd0621e249865b07ba3085c3
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-11-06 17:33:02 +0100

    Adding grades to user's score

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your Score: ");
+        string score = Console.ReadLine();
+
+        int x = int.Parse(score);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Your Grade is A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("Your Grade is B");
+
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("Your Grade is C");
+
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("Your Grade is D");
+
+        }
+        else
+        {
+            Console.WriteLine("Your Grade is F");
+
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed");
+        }
+        else
+        {
+            Console.WriteLine("Put in more effort next time");
+        }
+
     }
 }

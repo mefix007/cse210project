0dd3ff31936838d89b8dc9f9671f793b9c53abe7
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-11-06 18:48:13 +0100

    prep 3 assignment done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("what is the magic number?");
+        string magicNumber = Console.ReadLine();
+        Console.Write("what is your guess number?");
+        string guessNumber = Console.ReadLine();
+
+        int x = int.Parse(magicNumber);
+        int y = int.Parse(guessNumber);
+
+        if (x > y)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (x == y)
+        {
+            Console.WriteLine("You guess it");
+        }
+        else
+        {
+            Console.WriteLine("Lower");
+        }
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("what is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guess right!");
+            }
+        }
     }
+
 }

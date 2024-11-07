9abe440aaee92f82356b3f3fd503d4df76904a4c
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-11-07 19:39:31 +0100

    C# list prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int inputNumber = -1;
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter numbers: ");
+
+            string inputResponse = Console.ReadLine();
+            inputNumber = int.Parse(inputResponse);
+
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"the sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The highest number is: {max}");
+
+        int low = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < low)
+            {
+                low = number;
+            }
+        }
+        Console.WriteLine($"The lowest number is: {low}");
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
     }
 }

ef10168f7bccef3c6bc708ba55419cbc3baf9ca5
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-11-09 12:36:48 +0100

    prep 3 updated

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,26 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("what is the magic number?");
-        string magicNumber = Console.ReadLine();
-        Console.Write("what is your guess number?");
-        string guessNumber = Console.ReadLine();
+        // Console.Write("what is the magic number?");
+        // string magicNumber = Console.ReadLine();
+        // Console.Write("what is your guess number?");
+        // string guessNumber = Console.ReadLine();    
 
-        int x = int.Parse(magicNumber);
-        int y = int.Parse(guessNumber);
+        // int x = int.Parse(magicNumber);
+        // int y = int.Parse(guessNumber);
 
-        if (x > y)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (x == y)
-        {
-            Console.WriteLine("You guess it");
-        }
-        else
-        {
-            Console.WriteLine("Lower");
-        }
+        // if (x > y)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else if (x == y)
+        // {
+        //     Console.WriteLine("You guess it");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Lower");
+        // }
 
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1, 100);
@@ -33,7 +33,7 @@ class Program
 
         while (guess != number)
         {
-            Console.WriteLine("what is your guess?");
+            Console.Write("what is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
             if (number > guess)
